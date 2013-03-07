c9fe51c59602f4cf4c182be997049031f373992c
jiunsu <jiunsu@gmail.com>
2013-03-07 16:26:15 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mmlssp::Application.config.secret_token = 'e3bf9b1c57fbe381e16de46c59483b18e6ecba088a2fb313cef31a0d326a73d121d09ce150dcf6a13d552f99c3bf25cefe5684c6bc1caedb5c8d4d001eb9629d'
